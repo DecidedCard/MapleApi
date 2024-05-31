a39f885daa0a0717a0c1b5a77264346b387abeb6
heajun jeong <heajun35@gmail.com>
2024-06-01 01:59:57 +0900

    [Style] - 다크모드 디자인 변경

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -4,6 +4,9 @@ import { useEffect, useState } from "react";
 
 import { useTheme } from "next-themes";
 
+import { IoSunnyOutline } from "react-icons/io5";
+import { FaMoon } from "react-icons/fa";
+
 export function ThemeSwitcher() {
   const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
@@ -15,10 +18,28 @@ export function ThemeSwitcher() {
   if (!mounted) return null;
 
   return (
-    <div className="ml-72">
-      <button onClick={() => setTheme(theme == "light" ? "dark" : "light")}>
-        {`${theme == "light" ? "Dark" : "Light"} mode`}
-      </button>
-    </div>
+    <button
+      onClick={() => setTheme(theme == "light" ? "dark" : "light")}
+      className="flex justify-evenly items-center rounded-2xl overflow-hidden ease-in-out duration-300"
+    >
+      <div
+        className={`w-14 h-10 flex justify-center items-center ${
+          theme == "light"
+            ? "bg-slate-600 text-gray-300"
+            : "bg-gray-300 text-gray-700"
+        }`}
+      >
+        <FaMoon className="w-10 h-6" />
+      </div>
+      <div
+        className={`w-14 h-10 flex justify-center items-center ${
+          theme == "light"
+            ? "bg-gray-300 text-gray-700"
+            : "bg-white text-black "
+        }`}
+      >
+        <IoSunnyOutline className=" w-10 h-8" />
+      </div>
+    </button>
   );
 }
