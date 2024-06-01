7b3a4f15b4be0ff26023dac7527bf73b6381671d
heajun jeong <heajun35@gmail.com>
2024-06-01 23:55:23 +0900

    [Feat] - side navigation bar 제작

--- src/components/SideNavigationBar.tsx
+++ src/components/SideNavigationBar.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { ThemeSwitcher } from "./ThemeSwitcher";
+import Link from "next/link";
+import { IoExitOutline } from "react-icons/io5";
 
 const SideNavigationBar = ({
   sideNavigationBar,
@@ -11,6 +13,14 @@ const SideNavigationBar = ({
   const [animationCheck, setAnimationCheck] = useState(false);
   const mouseClickCheckRef = useRef<HTMLDivElement>(null);
 
+  const sidebarOffHandler = useCallback(() => {
+    setAnimationCheck(true);
+    setTimeout(() => {
+      setAnimationCheck(false);
+      setNavigationBar(false);
+    }, 750);
+  }, [setNavigationBar]);
+
   useEffect(() => {
     document.addEventListener("mousedown", (e) => {
       if (
@@ -18,27 +28,30 @@ const SideNavigationBar = ({
         mouseClickCheckRef.current &&
         !mouseClickCheckRef.current.contains(e.target as Node)
       ) {
-        setAnimationCheck(true);
-        setTimeout(() => {
-          setAnimationCheck(false);
-          setNavigationBar(false);
-        }, 750);
+        sidebarOffHandler();
       }
     });
-  }, [sideNavigationBar, setNavigationBar]);
+  }, [sideNavigationBar, sidebarOffHandler]);
 
   return (
     <nav
       ref={mouseClickCheckRef}
-      className={`absolute right-0 top-0 z-10 w-72 h-full bg-primary-100 ${
+      className={`absolute right-0 top-0 z-10 w-72 h-96 bg-primary-50 ${
         sideNavigationBar && !animationCheck && "animate-sideOn"
       } ${sideNavigationBar && animationCheck && "animate-sideOff"}`}
     >
       <ol className="flex flex-col items-start gap-4 m-2 text-2xl font-medium">
-        <ol>로그인</ol>
-        <ol>회원가입</ol>
+        <Link href={"/login"} className="w-full">
+          <ol className="bg-default-50">로그인</ol>
+        </Link>
+        <Link href={"/create_account"} className="w-full">
+          <ol className="bg-default-50">회원가입</ol>
+        </Link>
       </ol>
-      <ThemeSwitcher />
+      <div className="absolute bottom-0 right-1 flex justify-between w-[96%]">
+        <IoExitOutline onClick={sidebarOffHandler} className="cursor-pointer" />
+        <ThemeSwitcher />
+      </div>
     </nav>
   );
 };
