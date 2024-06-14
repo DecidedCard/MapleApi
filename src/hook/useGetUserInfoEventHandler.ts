af868ce4d490e1a6415a97cf761d9b808227dfd7
heajun jeong <heajun35@gmail.com>
2024-06-14 17:26:37 +0900

    [Feat] - union ranking 유저 클릭시 검색되게 작성

--- src/hook/useGetUserInfoEventHandler.ts
+++ src/hook/useGetUserInfoEventHandler.ts
@@ -4,11 +4,11 @@ import { SESSIONSTORAGE_KEY } from "@/util/sessionStorrageKey";
 import getUserInfo from "@/util/userInfo";
 import { useRouter } from "next/navigation";
 
-const useGetUserInfoEventHandler = (userName: string) => {
+const useGetUserInfoEventHandler = () => {
   const { setInfo } = useUserInfoStore();
   const router = useRouter();
 
-  const onClickGetUserInfoHandler = async () => {
+  const onClickGetUserInfoHandler = async (userName: string) => {
     setInfo(null);
     sessionStorage.setItem(SESSIONSTORAGE_KEY.userName, userName);
     const userInfo = await getUserInfo(userName);
