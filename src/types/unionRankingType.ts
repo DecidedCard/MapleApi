d44bf5f112d9000ab8d23941146ea5730516c4da
heajun jeong <heajun35@gmail.com>
2024-06-14 16:08:25 +0900

    [Feat] - union ranking type 정의

---
+++ src/types/unionRankingType.ts
@@ -0,0 +1,14 @@
+export type UnionRanking = {
+  ranking: [
+    {
+      date: string;
+      ranking: number;
+      character_name: string;
+      world_name: string;
+      class_name: string;
+      sub_class_name: string;
+      union_level: number;
+      union_power: string;
+    }
+  ];
+};
